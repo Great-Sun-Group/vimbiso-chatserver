a2a1b8753d46a5eac03abb4cf945dcaba97cca21
Ryan Luke Watson <85743878+ryanlukewatson@users.noreply.github.com>
2025-01-22 17:00:20 +0000

    remove redis persistence traces

--- terraform/modules/efs/outputs.tf
+++ terraform/modules/efs/outputs.tf
@@ -36,13 +36,3 @@ output "app_access_point_arn" {
   description = "ARN of the app data access point"
   value       = aws_efs_access_point.app_data.arn
 }
-
-output "redis_state_access_point_id" {
-  description = "ID of the Redis state access point"
-  value       = aws_efs_access_point.redis_state.id
-}
-
-output "redis_state_access_point_arn" {
-  description = "ARN of the Redis state access point"
-  value       = aws_efs_access_point.redis_state.arn
-}
