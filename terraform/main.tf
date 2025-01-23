46ef419561bbf177bafc7b518f7ff559d62c6238
Ryan Luke Watson <85743878+ryanlukewatson@users.noreply.github.com>
2025-01-23 01:44:26 +0000

    step 2, take 2

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,7 +18,7 @@ module "route53_cert" {
 
   environment        = var.environment
   domain_name       = "${local.current_env.subdomain}.${local.current_env.domain_base}"
-  create_dns_records = false  # Will be changed to true after NS records are configured
+  create_dns_records = true  # NS records are now configured in root zone
   tags              = local.common_tags
 }
 
@@ -149,7 +149,7 @@ module "route53_dns" {
 
   environment        = var.environment
   domain_name       = "${local.current_env.subdomain}.${local.current_env.domain_base}"
-  create_dns_records = false  # Will be changed to true after NS records are configured
+  create_dns_records = true  # NS records are now configured in root zone
   alb_dns_name      = module.loadbalancer.alb_dns_name
   alb_zone_id       = module.loadbalancer.alb_zone_id
   health_check_id   = module.health_checks.health_check_id
