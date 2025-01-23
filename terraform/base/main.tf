980a0ba2cb5914fc43c8c4f689c9609ae7a2208b
Ryan Luke Watson <85743878+ryanlukewatson@users.noreply.github.com>
2025-01-23 22:11:42 +0000

    another debug

--- terraform/base/main.tf
+++ terraform/base/main.tf
@@ -212,18 +212,6 @@ resource "aws_lb_target_group" "app" {
   }
 }
 
-# HTTP Listener
-resource "aws_lb_listener" "http" {
-  load_balancer_arn = aws_lb.main.arn
-  port              = "80"
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.app.arn
-  }
-}
-
 # ECS Cluster
 resource "aws_ecs_cluster" "main" {
   name = "vimbiso-pay-cluster-${var.environment}"
