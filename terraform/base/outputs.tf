5d0b328c84249c08c0a6d94cd3d36f935ba6ee29
Ryan Luke Watson <85743878+ryanlukewatson@users.noreply.github.com>
2025-01-23 22:13:09 +0000

    bug

--- terraform/base/outputs.tf
+++ terraform/base/outputs.tf
@@ -52,8 +52,3 @@ output "target_group_arn" {
   description = "ARN of the ALB target group"
   value       = aws_lb_target_group.app.arn
 }
-
-output "http_listener_arn" {
-  description = "ARN of the HTTP listener"
-  value       = aws_lb_listener.http.arn
-}
