1a69d4e0cbf0c674b6a64c50a76e308df6bf2633
Ryan Luke Watson <85743878+ryanlukewatson@users.noreply.github.com>
2025-01-24 18:26:32 +0000

    app module trim dependencies/outputs

--- terraform/app/outputs.tf
+++ terraform/app/outputs.tf
@@ -1,13 +1,3 @@
-output "cluster_name" {
-  description = "Name of the ECS cluster"
-  value       = aws_ecs_cluster.main.name
-}
-
-output "cluster_arn" {
-  description = "ARN of the ECS cluster"
-  value       = aws_ecs_cluster.main.arn
-}
-
 output "service_name" {
   description = "Name of the ECS service"
   value       = aws_ecs_service.app.name
